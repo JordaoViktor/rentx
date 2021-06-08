787f8e54905f632d14361af957ce70292017888f
JordaoViktor <jordaovpinheiro@gmail.com>
2021-06-08 03:21:36 -0300

    feat: initial setup

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+function Home() {
+  return (
+    <Container>
+      <Text>Hello</Text>
+    </Container>
+  );
+};
+
+export default Home;
