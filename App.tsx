1b1c3e9242ff2d802dd2b9d2613b9e5e40f0c17c
JordaoViktor <jordaovpinheiro@gmail.com>
2021-06-11 00:56:12 -0300

    feat: CardDetails screen was created and BackButton component

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ import {
   Archivo_500Medium,
   Archivo_600SemiBold,
 } from "@expo-google-fonts/archivo";
-import Home from "./src/screens/Home";
+import { CarDetails } from "./src/screens/CarDetails";
 import theme from "./src/styles/theme";
 
 export default function App() {
@@ -28,7 +28,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <CarDetails />
     </ThemeProvider>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { StatusBar } from "react-native";
 import Logo from "../../assets/logo.svg";
-import { Container, Header, HeaderContent, TotalCars } from "./styles";
+import { Container, Header, HeaderContent, TotalCars, CarList } from "./styles";
 import { RFValue } from "react-native-responsive-fontsize";
 import { Car } from "../../components/Car";
 
-function Home() {
+export function Home() {
   const carData = {
     brand: "Audi",
     name: "RS 6 Coupé",
@@ -29,9 +29,11 @@ function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
-      <Car data={carData} />
+      <CarList
+        data={[1, 2, 3, 4, 5, 6, 7]}
+        keyExtractor={(item) => String(item)}
+        renderItem={({ item }) => <Car data={carData} />}
+      />
     </Container>
   );
 }
-
-export default Home;
