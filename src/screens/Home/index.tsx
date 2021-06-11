0228114b1b5592a360a7a89a73c88abd3003f0d0
JordaoViktor <jordaovpinheiro@gmail.com>
2021-06-10 23:26:31 -0300

    feat: Header component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { StatusBar } from "react-native";
 import Logo from "../../assets/logo.svg";
-import { Container, Header } from "./styles";
+import { Container, Header, HeaderContent, TotalCars } from "./styles";
+import { RFValue } from "react-native-responsive-fontsize";
 
 function Home() {
   return (
@@ -12,7 +13,10 @@ function Home() {
         translucent
       />
       <Header>
-        <Logo />
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
       </Header>
     </Container>
   );
