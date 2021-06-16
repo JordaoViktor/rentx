0800b471e94fd33237f3ec35d7a3c36847e80103
JordaoViktor <jordaovpinheiro@gmail.com>
2021-06-16 00:02:47 -0300

    feat: server.json as 'api'

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -5,9 +5,11 @@ import { CarDetails } from "../screens/CarDetails";
 import { Scheduling } from "../screens/Scheduling";
 import { SchedulingDetails } from "../screens/SchedulingDetails";
 import { SchedulingComplete } from "../screens/SchedulingComplete";
-const { Navigator, Screen } = createNativeStackNavigator();
 import { enableScreens } from "react-native-screens";
+const { Navigator, Screen } = createNativeStackNavigator();
+
 enableScreens();
+
 export function StackRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
