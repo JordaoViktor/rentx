08c58f6340fe371483c04d102f6fa535c6b4026b
JordaoViktor <jordaovpinheiro@gmail.com>
2021-06-15 23:53:03 -0300

    feat: react-native-screens to improve the routes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "react-native-screens/native-stack";
 import { Home } from "../screens/Home";
 import { CarDetails } from "../screens/CarDetails";
 import { Scheduling } from "../screens/Scheduling";
 import { SchedulingDetails } from "../screens/SchedulingDetails";
 import { SchedulingComplete } from "../screens/SchedulingComplete";
-const { Navigator, Screen } = createStackNavigator();
-
+const { Navigator, Screen } = createNativeStackNavigator();
+import { enableScreens } from "react-native-screens";
+enableScreens();
 export function StackRoutes() {
   return (
-    <Navigator headerMode="none">
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="Home" component={Home} />
       <Screen name="CarDetails" component={CarDetails} />
       <Screen name="Scheduling" component={Scheduling} />
